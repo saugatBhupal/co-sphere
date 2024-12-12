88906c36c32ede313588085d22daf6413ebbcaeb
Saugat Singh <saugatbhupal@bhunet.com>
2024-12-12 23:49:00 +0545

    created choice chips for interests page

---
+++ lib/views/signup/password_screen.dart
@@ -0,0 +1,81 @@
+import 'package:cosphere/config/app_routes/app_routes.dart';
+import 'package:cosphere/constants/app_assets.dart';
+import 'package:cosphere/constants/app_strings.dart';
+import 'package:cosphere/widgets/appbar/authentication_appbar.dart';
+import 'package:cosphere/widgets/buttons/dark_rounded_button.dart';
+import 'package:cosphere/widgets/input_fields/password_field.dart';
+import 'package:cosphere/widgets/textSpan.dart/account_textspan.dart';
+import 'package:cosphere/widgets/textSpan.dart/auth_message.dart';
+import 'package:flutter/material.dart';
+
+class PasswordScreen extends StatefulWidget {
+  const PasswordScreen({super.key});
+
+  @override
+  State<PasswordScreen> createState() => _PasswordScreenState();
+}
+
+class _PasswordScreenState extends State<PasswordScreen> {
+  late final TextEditingController _passwordController;
+  late final TextEditingController _confirmController;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordController = TextEditingController();
+    _confirmController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _passwordController.dispose();
+    _confirmController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 26),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const AuthenticationAppbar(),
+              const SizedBox(height: 20),
+              const AuthMessage(
+                  title: AppStrings.passwordTitle,
+                  subtitle: AppStrings.passSubtitle),
+              const SizedBox(height: 40),
+              PasswordField(
+                passwordController: _passwordController,
+                label: AppStrings.password,
+                icon: AppIcons.user,
+              ),
+              const SizedBox(height: 30),
+              PasswordField(
+                passwordController: _confirmController,
+                label: "${AppStrings.confirm} ${AppStrings.password}",
+                icon: AppIcons.user,
+              ),
+              const SizedBox(height: 40),
+              DarkRoundedButton(
+                title: AppStrings.continueBtn,
+                onPressed: () =>
+                    Navigator.of(context).pushNamed(AppRoutes.interest),
+              ),
+              const SizedBox(height: 15),
+              AccountTextspan(
+                infoText: AppStrings.haveAccount,
+                functionText: AppStrings.signin,
+                onPressed: () =>
+                    Navigator.of(context).pushNamed(AppRoutes.signin),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/signup/signup_screen.dart
@@ -0,0 +1,93 @@
+import 'package:cosphere/config/app_routes/app_routes.dart';
+import 'package:cosphere/constants/app_strings.dart';
+import 'package:cosphere/widgets/appbar/authentication_appbar.dart';
+import 'package:cosphere/widgets/buttons/dark_rounded_button.dart';
+import 'package:cosphere/widgets/input_fields/dob_field.dart';
+import 'package:cosphere/widgets/input_fields/email_field.dart';
+import 'package:cosphere/widgets/input_fields/input_field.dart';
+import 'package:cosphere/widgets/input_fields/phone_field.dart';
+import 'package:cosphere/widgets/textSpan.dart/account_textspan.dart';
+import 'package:cosphere/widgets/textSpan.dart/auth_message.dart';
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  late final TextEditingController _emailController;
+  late final TextEditingController _nameController;
+  late final TextEditingController _phoneController;
+  late final TextEditingController _dobController;
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _nameController = TextEditingController();
+    _phoneController = TextEditingController();
+    _dobController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _nameController.dispose();
+    _phoneController.dispose();
+    _dobController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 26),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const AuthenticationAppbar(),
+              const SizedBox(height: 20),
+              const AuthMessage(
+                  title: AppStrings.signupTitle,
+                  subtitle: AppStrings.signinSubtitle),
+              const SizedBox(height: 40),
+              EmailField(
+                emailController: _emailController,
+                label: AppStrings.email,
+              ),
+              const SizedBox(height: 25),
+              InputField(
+                textController: _nameController,
+                label: AppStrings.name,
+              ),
+              const SizedBox(height: 25),
+              PhoneField(
+                phoneController: _phoneController,
+                label: AppStrings.phone,
+              ),
+              const SizedBox(height: 25),
+              DobField(dobController: _dobController),
+              const SizedBox(height: 45),
+              DarkRoundedButton(
+                title: AppStrings.continueBtn,
+                onPressed: () =>
+                    Navigator.of(context).pushNamed(AppRoutes.location),
+              ),
+              const SizedBox(height: 15),
+              AccountTextspan(
+                infoText: AppStrings.haveAccount,
+                functionText: AppStrings.signin,
+                onPressed: () =>
+                    Navigator.of(context).pushNamed(AppRoutes.signin),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
