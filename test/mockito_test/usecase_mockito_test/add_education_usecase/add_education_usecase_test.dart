7062deda9a45aca9ba485fc720fc226c1c938561
Saugat <saugatbhupal@bhunet.com>
2025-02-07 11:36:49 +0545

    add education usecase testing

--- test/mockito_test/usecase_mockito_test/add_education_usecase/add_education_usecase_test.dart
+++ test/mockito_test/usecase_mockito_test/add_education_usecase/add_education_usecase_test.dart
@@ -1,56 +1,61 @@
 import 'package:cosphere/src/core/error/failure.dart';
+import 'package:cosphere/src/features/profile/data/dto/education/add_education_req_dto.dart';
 import 'package:cosphere/src/features/profile/data/dto/experience/add_experience_req_dto.dart';
+import 'package:cosphere/src/features/profile/domain/entities/education.dart';
 import 'package:cosphere/src/features/profile/domain/entities/experience.dart';
 import 'package:cosphere/src/features/profile/domain/repositories/profile_repository.dart';
-import 'package:cosphere/src/features/profile/domain/usecases/add_experience_usecase.dart';
+import 'package:cosphere/src/features/profile/domain/usecases/add_education_usecase.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import 'add_experience_usecase_test.mocks.dart';
+import 'add_education_usecase_test.mocks.dart';
 
 @GenerateMocks([ProfileRepository])
 void main() {
   late MockProfileRepository mockProfileRepository;
-  late AddExperienceUsecase addExperienceUsecase;
+  late AddEducationUsecase addEducationUsecase;
 
   setUp(() {
     mockProfileRepository = MockProfileRepository();
-    addExperienceUsecase =
-        AddExperienceUsecase(profileRepository: mockProfileRepository);
+    addEducationUsecase =
+        AddEducationUsecase(profileRepository: mockProfileRepository);
   });
-  final reqDto = AddExperienceReqDto(
+
+  final reqDto = AddEducationReqDto(
     email: "test@gmail.com",
-    position: "testPosition",
+    degree: "testDegree",
     organization: "testOrganization",
-    status: "testStatus",
     from: DateTime.now(),
     to: DateTime.now(),
   );
-
   test('should return user entity when Login is successful', () async {
     // Arrange
 
-    when(mockProfileRepository.addExperience(reqDto))
-        .thenAnswer((_) async => Right(Experience.initial()));
+    when(mockProfileRepository.addEducation(reqDto))
+        .thenAnswer((_) async => Right(Education.initial()));
     // Act
-    final result = await addExperienceUsecase.call(reqDto);
+    final result = await addEducationUsecase.call(reqDto);
     // Assert
-    expect(result, Right(Experience.initial()));
-    verify(mockProfileRepository.addExperience(reqDto)).called(1);
+    expect(result, Right(Education.initial()));
+    verify(mockProfileRepository.addEducation(reqDto)).called(1);
     verifyNoMoreInteractions(mockProfileRepository);
   });
+
   test('should return Failure when an User is not found', () async {
     // Arrange
     const failure = Failure(message: "User not found");
-    when(mockProfileRepository.addExperience(reqDto))
+
+    when(mockProfileRepository.addEducation(reqDto))
         .thenAnswer((_) async => const Left(failure));
+
     // Act
-    final result = await addExperienceUsecase.call(reqDto);
+    final result = await addEducationUsecase.call(reqDto);
+
     // Assert
     expect(result, const Left(failure));
-    verify(mockProfileRepository.addExperience(reqDto)).called(1);
+    verify(mockProfileRepository.addEducation(reqDto)).called(1);
     verifyNoMoreInteractions(mockProfileRepository);
   });
 }
