e5e42f804567be4b0f7a3af0138f5749906db8c8
Saugat <saugatbhupal@bhunet.com>
2025-02-07 10:37:17 +0545

    create password usecase mock test

---
+++ test/mockito_test/usecase_mockito_test/create_password_usecase/create_password_usecase_test.dart
@@ -0,0 +1,55 @@
+import 'package:cosphere/src/core/error/failure.dart';
+import 'package:cosphere/src/features/authentication/data/dto/create_password/create_password_request_dto.dart';
+import 'package:cosphere/src/features/authentication/domain/repositories/auth_repository.dart';
+import 'package:cosphere/src/features/authentication/domain/usecases/create_password_usecase.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'create_password_usecase_test.mocks.dart';
+
+@GenerateMocks([AuthRepository])
+void main() {
+  late CreatePasswordUsecase createPasswordUsecase;
+  late MockAuthRepository mockAuthRepository;
+
+  setUp(() {
+    mockAuthRepository = MockAuthRepository();
+    createPasswordUsecase =
+        CreatePasswordUsecase(authRepository: mockAuthRepository);
+  });
+  test('should return success message when signup is successful', () async {
+    final requestDto =
+        CreatePasswordRequestDto(email: 'test@gmail.com', password: 'test@123');
+    const successMessage = 'Password Created Successfully';
+    when(mockAuthRepository.createPassword(any))
+        .thenAnswer((_) async => const Right(successMessage));
+    // Act
+    final result = await createPasswordUsecase(requestDto);
+
+    // Assert
+    expect(result, const Right(successMessage));
+
+    // Verify
+    verify(mockAuthRepository.createPassword(requestDto)).called(1);
+    verifyNoMoreInteractions(mockAuthRepository);
+  });
+
+  test('should return Failure when the Email is incorrect', () async {
+    // Arrange
+    final requestDto = CreatePasswordRequestDto(
+        email: 'test123@gmail.com', password: 'test@123');
+    const failure = Failure(message: "Email does not exist");
+
+    when(mockAuthRepository.createPassword(requestDto))
+        .thenAnswer((_) async => const Left(failure));
+    // Act
+    final result = await createPasswordUsecase.call(requestDto);
+
+    // Assert
+    expect(result, const Left(failure));
+    verify(mockAuthRepository.createPassword(requestDto)).called(1);
+    verifyNoMoreInteractions(mockAuthRepository);
+  });
+}
